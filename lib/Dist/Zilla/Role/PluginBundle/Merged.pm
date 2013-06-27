de96bf5a4138c9aa65cc9ddb4f1c9a1221e006db
Brendan Byrd <Perl@ResonatorSoft.org>
2013-06-27 18:27:17 -0400

    Add config_short_merge option Add warnings for undesired mistakes in the PB build Add extra docs

--- lib/Dist/Zilla/Role/PluginBundle/Merged.pm
+++ lib/Dist/Zilla/Role/PluginBundle/Merged.pm
@@ -3,11 +3,12 @@ package Dist::Zilla::Role::PluginBundle::Merged;
 # VERSION
 # ABSTRACT: Mindnumbingly easy way to create a PluginBundle
 
-use sanity;
 use MooseX::Role::Parameterized;
+use sanity;
 
 use Class::Load;
 use Storable 'dclone';
+use Scalar::Util 'blessed';
 
 use String::RewritePrefix 0.005 rewrite => {
    -as => '_section_class',
@@ -15,6 +16,14 @@ use String::RewritePrefix 0.005 rewrite => {
       ''  => 'Dist::Zilla::Plugin::',
       '@' => 'Dist::Zilla::PluginBundle::',
       '=' => ''
+   },
+};
+use String::RewritePrefix 0.005 rewrite => {
+   -as => '_plugin_name',
+   prefixes => {
+      'Dist::Zilla::Plugin::'       => '',
+      'Dist::Zilla::PluginBundle::' => '@',
+      '' => '=',
    },
 };
 
@@ -50,13 +59,22 @@ role {
 
       my @config;
       foreach my $name (@list) {
-         if (ref $name) {
-            $arg = $name;
+         if (my $ref = ref $name) {
+            if    ($ref eq 'HASH')  { $arg = $name; }
+            elsif ($ref eq 'ARRAY') { $self->add_plugins($name); }
+            else                    { die "Cannot pass $ref to add_merged"; }
+
             next;
          }
 
          my $class = _section_class($name);
          Class::Load::load_class($class);
+
+         # check mv_plugins list to make sure the class was passed
+         unless (grep { $_ eq $name } @{ $p->mv_plugins }) {
+            say $self->_fake_log_prefix." $name has MVPs, but was never passed in the mv_plugins list"
+               if ($class->can('mvp_multivalue_args') and scalar($class->mvp_multivalue_args));
+         }
 
          # handle mvp_aliases
          my %aliases = ();
@@ -91,6 +109,47 @@ role {
 
       return $args;
    };
+
+   method config_short_merge => sub {
+      my ($self, $mod_list, $config_hash) = @_;
+
+      $mod_list = [ $mod_list ] unless ref $mod_list;
+
+      # figure out if the options are actually going to work
+      foreach my $name (@$mod_list) {
+         next if $name =~ /^\@/;
+
+         my $class = _section_class($name);
+         Class::Load::load_class($class);
+
+         # handle mvp_aliases
+         my %aliases = ();
+         %aliases = %{$class->mvp_aliases} if $class->can('mvp_aliases');
+
+         foreach my $k (keys %$config_hash) {
+            say $self->_fake_log_prefix." $name doesn't support argument '$k' as a standard attribute.  (Maybe you should use explicit arg passing?)"
+               unless $class->can( $aliases{$k} || $k );
+         }
+      }
+
+      return (
+         { %$config_hash, %{$self->payload} },
+         @$mod_list,
+         $self->payload,
+      );
+   };
+
+   # written entirely in hackish
+   method _fake_log_prefix => sub {
+      my $self = shift;
+      my $plugin_name = _plugin_name(blessed $self);
+
+      my @parts;
+      push @parts, $self->name  if $self->name;
+      push @parts, $plugin_name unless ($self->name =~ /\Q$plugin_name\E$/);
+
+      '['.join('/', @parts).']';
+   };
 };
 
 42;
@@ -122,6 +181,7 @@ __END__
          qw( ArglessPlugin1 ArglessPlugin2 ),
          $self->config_rename(plugin_dupearg => 'dupearg', removearg => undef),
          qw( Plugin2 ),
+         $self->config_short_merge(['Plugin3', 'Plugin4'], { defaultarg => 1 }),
       );
    }
 
@@ -154,6 +214,9 @@ it processes.  This is useful for changing the options "on-the-fly" as plugins g
 replacement is done in order, and the changes will persist until it reaches the end of the list, or
 receives another replacement.
 
+If passed an arrayref, it will be directly passed to add_plugins.  Useful for plugins that use BUILDARGS
+or some other non-standard configuration setup.
+
 == config_rename
 
 This method is sort of like the [config_slice|Dist::Zilla::Role::PluginBundle::Easy/config_slice] method,
@@ -174,6 +237,21 @@ specific option for the plugin "Foobar" that doesn't clash with {arg1} on plugin
 Any destination options are replaced.  Also, if the destination value is undef (or non-true), the key will
 simply be deleted.  Keep in mind that this is all a clone of the payload, so extra calls to this method
 will still start out with the original payload.
+
+== config_short_merge
+
+Like {config_rename}, this is meant to be used within an {add_merged} block.  It takes either a single
+plugin (scalar) or multiple ones (arrayref) as the first parameter, and a hashref of argument/value pairs
+as the second one.  This will merge in your own argument/value pairs to the existing payload, pass the
+module list, and then pass the original payload back.  For example:
+
+   $self->add_merged(
+      $self->config_short_merge(['Baz', 'Foobar'], { arg1 => 1 }),  # these two plugins have payload + arg1
+      'Boom',  # only has the original payload
+   );
+
+Furthermore, the argument hash is expanded prior to the payload, so they can be overwritten by the payload.
+Think of this as default arguments to pass to the plugins.
 
 = ROLE PARAMETERS
 
@@ -200,5 +278,50 @@ as long as you identify all of the plugins that would have multiple values, your
 
 If you need to identify any extra parameters as MVPs (like your own custom MVPs or "dupe preventing" parameters
 that happen to be MVPs), you should consider combining {mv_plugins} with an {after mvp_multivalue_args} sub.
+
+= SUMMARY OF PARAMETERS
+
+Here are all of the different options you can pass to {add_merged}:
+
+   $self->add_merged(
+      ### SCALARs ###
+      # These are all passed to add_plugins with an implicit payload
+      'Plugin',
+      '@PluginBundle',
+      '=Dist::Zilla::Bizarro::Plugin',  # explicit class of plugin
+
+      ### ARRAYs ###
+      # These are all passed to add_plugins with an explicit payload
+      ['Plugin'],
+      ['Plugin', 'NewName'],
+      ['Plugin', \%new_payload ],
+      ['Plugin', 'NewName', \%new_payload ],
+
+      ### HASHs ###
+      {},              # force no options until reset
+      $self->payload,  # reset to original payload
+      \%new_payload,   # only pass those arg/value pairs as the payload
+
+      $self->config_slice(qw( arg1 arg2 )),                    # only pass those args -from- the payload
+      $self->config_slice('arg1', { foobar_arg2 => 'arg2' }),  # only pass those args -from- the payload (with arg renaming)
+
+      $self->config_rename(foobar_arg1 => 'arg1'),             # rename args in the payload (and pass everything else)
+      $self->config_rename(killme => ''),                      # remove args in the payload (and pass everything else)
+
+      ### Combinations ###
+      $self->config_short_merge('Plugin', \%add_on_payload),   # add args to the payload, pass to Plugin, and reset to original
+      $self->config_short_merge(
+         [ qw( Plugin1 Plugin2 ) ],    # add args to the payload, pass to plugin list, and reset to original payload
+         \%add_on_payload
+      ),
+   );
+
+= CAVEATS
+
+* Plugins that use non-standard payload methods will not be passed their options via {add_merged}, unless passed
+an arrayref to {add_merged} with an specific payload.  The {config_merge} method will warn you of this, because
+it knows that you really want to use that argument.  Others will not.
+
+* Doing things more implicitly grants greater flexibility while sacrificing control.  YMMV.
 
 =end wikidoc
